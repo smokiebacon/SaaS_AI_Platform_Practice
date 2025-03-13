1c9b46e152e14b14aa10b72cfaee933c7bf71a7e
Abraham <smokiebacon@gmail.com>
2025-03-12 17:52:26 -0700

    setup project structure

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+//{params : id}
+const TransformationsIDPage = () => {
+  return <div>TransformationsIDPage</div>
+}
+
+export default TransformationsIDPage
+// localhost:3000/transformations/[id]
